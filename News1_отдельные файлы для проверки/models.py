0080d485024180226f64d9696388147021cc8393
vladstulov <78933990+vladstulov@users.noreply.github.com>
2023-01-29 13:56:40 +0300

    Update models.py

--- News1_отдельные файлы для проверки/models.py
+++ News1_отдельные файлы для проверки/models.py
@@ -5,16 +5,10 @@ from django.core.validators import MinValueValidator
 
 # Создаём модель новости
 class News(models.Model):
-    title = models.CharField(
-        max_length=50,
-        unique=True,
-    )
+    title = models.CharField(max_length=50, unique=True)
     text = models.TextField()
-
     dateCreation = models.DateTimeField(auto_now_add=True)
 
-
-
     def __str__(self):
-        return f'{self.title.title()}. Дата публикации: {self.dateCreation.strftime("%d-%b-%Y %H:%M")}. {self.text}...'
+        return f'{self.title}. Дата публикации: {self.dateCreation.strftime("%d-%b-%Y %H:%M")}. {self.text}...'
 
